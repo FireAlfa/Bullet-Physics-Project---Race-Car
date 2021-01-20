30ea4fa997d3fa902a3ce44de1ef3db43c07958d
himar33 <himar33@users.noreply.github.com>
2021-01-20 11:51:16 +0100

    Title fixed

--- BulletPhysicsProject_RaceCar/ModulePlayer.cpp
+++ BulletPhysicsProject_RaceCar/ModulePlayer.cpp
@@ -220,7 +220,24 @@ update_status ModulePlayer::Update(float dt)
 	vehicle->Render();
 
 	char title[80];
-	sprintf_s(title, "%.1f Km/h, Gear: %d, Engine: %d", vehicle->GetKmh(), gearState, engine);
+	if (engine == true)
+	{
+		switch (gearState)
+		{
+		case ModulePlayer::PARKING:
+			sprintf_s(title, "%.1f Km/h, Gear: Parking, Engine: On", vehicle->GetKmh());
+			break;
+		case ModulePlayer::DRIVE:
+			sprintf_s(title, "%.1f Km/h, Gear: Drive, Engine: On", vehicle->GetKmh());
+			break;
+		case ModulePlayer::REVERSE:
+			sprintf_s(title, "%.1f Km/h, Gear: Reverse, Engine: On", vehicle->GetKmh());
+			break;
+		default:
+			break;
+		}
+	}
+	else sprintf_s(title, "%.1f Km/h, Gear: None, Engine: Off", vehicle->GetKmh(), gearState);
 	App->window->SetTitle(title);
 
 	return UPDATE_CONTINUE;
